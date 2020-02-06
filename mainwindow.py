7674d5fe691e54f8144f3ab742ea352d07610af5
Juanma Sánchez <juxmix@gmail.com>
2020-02-06 09:53:59 +0100

    automatic author tag updated

--- mainwindow.py
+++ mainwindow.py
@@ -1,18 +1,18 @@
 '''
 Created on 7 de febr. 2019
 
-@author: super
+@author: juxmix
 '''
 import Tkinter, tkFileDialog
 from jpgrenamer import jpgrenamer
 import os
 
 class mainwindow:
- 
+
     def __init__(self):
         self._dirSelected = None
         self._jpgSelected = None
-        
+
         self._top = Tkinter.Tk()
         self._top.geometry("390x100")
 
@@ -39,10 +39,10 @@ class mainwindow:
         self._btnSalir = Tkinter.Button(self._frmBotones, text ="Salir", command = quit)
         self._btnNormaliza.grid(column=0, row = 1)
         self._btnDesNormaliza.grid(column=1, row = 1)
-        self._btnSalir.grid(column=2, row = 1)        
+        self._btnSalir.grid(column=2, row = 1)
         #go
         self._top.mainloop()
-  
+
     def normaliza(self):
         if self._dirSelected:
             print(self._dirSelected)
@@ -55,36 +55,36 @@ class mainwindow:
             jpgimg = jpgrenamer(self._jpgSelected)
             self._jpgSelected = jpgimg.normalizeJpgName()
             self._txtJpg.delete(1.0, "end")
-            self._txtJpg.insert("end", self._jpgSelected)            
+            self._txtJpg.insert("end", self._jpgSelected)
             print(self._jpgSelected)
         else:
             print("Nada seleccionado")
-   
+
     def desNormaliza(self):
         if self._dirSelected:
             print(self._dirSelected)
             fotos = self.getJpgsInDir(self._dirSelected)
             for f in fotos:
                 jpgren = jpgrenamer(f)
-                jpgren.unNormalizeJpgName()                     
+                jpgren.unNormalizeJpgName()
         elif self._jpgSelected:
             #print(self._jpgSelected)
             jpgimg = jpgrenamer(self._jpgSelected)
             self._jpgSelected = jpgimg.unNormalizeJpgName()
             self._txtJpg.delete(1.0, "end")
-            self._txtJpg.insert("end", self._jpgSelected)            
+            self._txtJpg.insert("end", self._jpgSelected)
             print(self._jpgSelected)
         else:
             print("Nada seleccionado")
-    
+
     def getJpgsInDir(self, direc):
         ficheros = [os.path.abspath(x) for x in os.listdir(direc)]
         fotos = []
         for f in ficheros:
             if f.lower().endswith("jpg"):
-                fotos.append(f)    
+                fotos.append(f)
         return fotos
-        
+
     def selectDir(self):
         #self._top.withdraw()
         self._dirSelected = tkFileDialog.askdirectory()
@@ -94,7 +94,7 @@ class mainwindow:
         self._txtDir.insert("end", self._dirSelected)
         self._txtJpg.delete(1.0, "end")
         self._jpgSelected = None
-    
+
     def selectJpg(self):
         self._jpgSelected = tkFileDialog.askopenfilename()
         print("JPG: " + self._jpgSelected)
@@ -103,6 +103,6 @@ class mainwindow:
         self._txtJpg.insert("end", self._jpgSelected)
         self._txtDir.delete(1.0, "end")
         self._dirSelected = None
-        
+
 if __name__ == '__main__':
-    mw = mainwindow() 
+    mw = mainwindow()
